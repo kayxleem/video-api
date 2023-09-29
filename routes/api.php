b93f5df8b9c070e1a97ce267c1a063ecacbcd644
Oluwakayode Adegbuyi <kayode_adegbuyi@yahoo.com>
2023-09-30 00:53:27 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -19,9 +19,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/',[VideoController::class,'index']);
+Route::get('/',[VideoController::class,'index'])->name('get');
 Route::post('/',[VideoController::class,'store']);
 Route::get('/{id}',[VideoController::class,'show']);
-//Route::delete('/{id}',[VideoController::class,'destroy']);
-//Route::patch('/{id}',[VideoController::class,'update']);
-//Route::put('/{id}',[VideoController::class,'update']);
+Route::prefix('v1')->group(function(){
+    Route::get('/', [VideoController::class,'index'])->name('v1.index');
+    Route::post('/', [VideoController::class,'store'])->name('v1.store');
+    //Route::post('/auth/user/forgot-password', [ResetPasswordController::class, 'forgotPassword'])->name('user.forgotPassword');
+    //Route::post('/organization/staff/signup', [OrganizationController::class, 'createOrganizationUser']);
+});
+
